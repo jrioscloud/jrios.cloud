38576141d7ba7c4ae0a30e23ba4d3600b2bf3f6e
Jaime Rios <jaime.rios@hey.com>
2025-02-22 10:40:37 -0600

    refactor: remove Tailwind CSS directives from CSS files and integrate Alpine.js plugins for enhanced functionality

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Alpine from 'alpinejs'
+import collapse from '@alpinejs/collapse'
+import focus from '@alpinejs/focus'
+
 
 window.Alpine = Alpine
 
 Alpine.start()
+Alpine.plugin(collapse)
+Alpine.plugin(focus)
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
-
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
